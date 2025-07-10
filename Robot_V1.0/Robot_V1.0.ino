182afb4844d88fa2a687c7c0b4333348dab30df2
NaNaSikh <nanasixarulidze78@gmail.com>
2025-07-11 00:21:46 +0400

    commit

---
+++ Robot_V1.0/Robot_V1.0.ino
@@ -0,0 +1,22 @@
+#include <ESP32Servo.h>
+
+class WheelsMoror{
+
+}
+
+class WeaponMotor{
+
+}
+
+class RadioController{
+  
+}
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
